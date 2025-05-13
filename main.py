5a4d18ba06b030e115da4ccf1923411a36bd1112
jabrone51 <antmoriggia@yahoo.com>
2025-05-13 09:44:25 -0400

    books are referenced through relative file path, error handling added for wrong path and no path entered

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
+import sys
 from stats import get_book_text, get_word_count, get_character_count
     
 def main():
-    text = get_book_text("books/frankenstein.txt")
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    
+    book_path = sys.argv[1]
+
+    try:
+        text = get_book_text(book_path)
+    except FileNotFoundError:
+        print("Book Not Found")
+        sys.exit(1)
+
     word_count = get_word_count(text)
     char_count = get_character_count(text)
 
     print(f"""
 ========== BOOKBOT ==========
-Analyzing book found at book/frankenstein.txt...
+Analyzing book found at {book_path}...
 -------- Word Count ---------
 Found {word_count} total words
 ------ Character Count ------
